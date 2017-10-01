61190be8269a8b6b33bc5c5f185bbdc4b1331e52
Dmitry Ovsyanko <do_@pochta.ru>
2017-10-01 21:26:41 +0300

    'xsl' namespace prefix

--- xsl.pl
+++ xsl.pl
@@ -1,7 +1,10 @@
 use XML::LibXML;
 use XML::LibXSLT;
 
-print XML::LibXSLT -> new ()
+my $doc = XML::LibXSLT -> new ()
 	-> parse_stylesheet (XML::LibXML -> load_xml (location => $ARGV [0]))
-	-> transform        (XML::LibXML -> load_xml (IO => STDIN))
-	-> toString (2)
+	-> transform        (XML::LibXML -> load_xml (IO => STDIN));
+	
+$doc -> documentElement () -> setNamespace ('http://www.w3.org/1999/XSL/Transform', 'xsl', 0);
+
+print $doc -> toString (2);
